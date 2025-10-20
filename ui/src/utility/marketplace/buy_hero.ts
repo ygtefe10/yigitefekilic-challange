10465d7751d3f592ed2c86e303ff90c951c6e0ca
ygtefe10 <ygt-efe10@hotmail.com>
2025-10-21 01:24:18 +0300

    Update buy_hero.ts

--- ui/src/utility/marketplace/buy_hero.ts
+++ ui/src/utility/marketplace/buy_hero.ts
@@ -3,19 +3,17 @@ import { Transaction } from "@mysten/sui/transactions";
 export const buyHero = (packageId: string, listHeroId: string, priceInSui: string) => {
   const tx = new Transaction();
   
-  // TODO: Convert SUI to MIST (1 SUI = 1,000,000,000 MIST)
-    // Hints:
-    // const priceInMist = ?
-  // TODO: Split coin for exact payment
-    // Hints  
-    // Use tx.splitCoins(tx.gas, [priceInMist]) to create a payment coin
-    // const [paymentCoin] = ?
-  // TODO: Add moveCall to buy a hero
-  // Function: `${packageId}::marketplace::buy_hero`
-  // Arguments: listHeroId (object), paymentCoin (coin)
-    // Hints:
-    // Use tx.object() for the ListHero object
-    // Use the paymentCoin from splitCoins for payment
-    
+
+  const priceInMist = Number(priceInSui) * 1_000_000_000;
+  
+  const [paymentCoin] = tx.splitCoins(tx.gas, [priceInMist]);
+  tx.moveCall({
+   target: `${packageId}::marketplace::buy_hero`,
+   arguments: [
+     tx.object(listHeroId),
+     paymentCoin
+   ]
+  })
+
   return tx;
 };
